cf8c3ce7c41456647d3dc2c42fe0d140904b6c31
Luciano Penha <lucianopenha14@gmail.com>
2022-12-10 12:52:53 -0300

    Aula29

--- MYSQL/Exercicio04.sql
+++ MYSQL/Exercicio04.sql
@@ -1,5 +1,11 @@
-select pedido_id as numero_pedido, 
-SUM(quantidade * valor) as total_pedido
-from pedido_item group by pedido_id
-order by 2 desc
-limit 10
+CREATE TABLE `users` (
+	`email` VARCHAR(120) NOT NULL COLLATE 'utf8mb3_general_ci',
+	`nome` VARCHAR(45) NOT NULL COLLATE 'utf8mb3_general_ci',
+	`senha` VARCHAR(80) NOT NULL COLLATE 'utf8mb3_general_ci',
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`email`) USING BTREE
+)
+COLLATE='utf8mb3_general_ci'
+ENGINE=InnoDB
+;
